e12e5b81fffa7a12df628b9e5282f045cc16b2bc
Vidushi0808-hub <69456566+Vidushi0808-hub@users.noreply.github.com>
2020-09-01 15:29:11 +0530

    Add files via upload

---
+++ nested-for.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3
+#
+# SCRIPT: nested-for
+# AUTHOR: Vidushi Bansal
+# DATE:   01/09/2020
+# REV:    1.1.A (Valid are A, B, D, T and P)
+#          (For Alpha, Beta, Dev, Test and Production)
+#
+#
+# PLATFORM: UBUNTU
+#
+# PURPOSE: To create a nested for loop
+# REV LIST:
+#    DATE        : Date of revision
+#    BY          : AUTHOR OF MODIFICATION
+#    MODIFICATION: Describe the chages made. What do they enhance.
+#
+# set -n   # Uncomment to check script syntax, without execution.
+#          # NOTE: Do forget comment it back as it won't allow the
+#          # the script to execute.
+#
+# set +x   # Uncomment this for debugging this shell script.
+#
+#
+################################################################
+#          Define Files and Variables here                     #
+for a in [1, 2]:
+    for b in ['a', 'b']:
+        print (a, b)
+
+################################################################
+################################################################
+#          Define Functions here                               #
+
+################################################################
+################################################################
+#          Beginning of Main                                   #
+################################################################
+# End of script							#
+
+
